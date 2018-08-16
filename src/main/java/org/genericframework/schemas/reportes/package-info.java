1ad5febb7e1d0793af24674b1fb0fb889cc09e41
Efren Garcia <efren.garcia.guarneros@gmail.com>
2018-08-16 07:54:34 -0500

    Se elimina clases autogeneradas
    Se eliminan clases autogeneradas por el plugin de Maven.

--- src/main/java/org/genericframework/schemas/reportes/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
-// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2018.06.11 a las 11:18:30 PM CDT 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://genericframework.org/schemas/reportes", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.genericframework.schemas.reportes;
